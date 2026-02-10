a4e58740b58781850810ea3783f8676cf8f0f859
Aru <132051378+Aru-gxtx@users.noreply.github.com>
2026-02-10 18:54:25 +0800

    Fixed build

--- main.cpp
+++ main.cpp
@@ -11,19 +11,26 @@ using namespace std;
 int main() {
     srand(time(0));
 
-    int min_sec = 180;
+    int min_sec = 22.5;
     int max_sec = 360;
 
-    while (true) {
-        int random_sec = min_sec + (rand() % (max_sec - min_sec + 1));
-        
-        cout << "Next reminder in " << random_sec << " seconds.\n";
+    cout << "Reminder started. Waiting for the first interval...\n";
 
-        // Sleep first
-        this_thread::sleep_for(chrono::seconds(random_sec));
-        
-        // Then play sound
-        PlaySound(TEXT("harikitte-ikou-kitasan-black.mp3"), NULL, SND_FILENAME | SND_ASYNC);
+    while (true) {
+        // Calculate random time inside the loop so it changes every time
+        int random_sec = min_sec + (rand() % (max_sec - min_sec + 1));
+
+        for (int i = random_sec; i > 0; --i) {
+            cout << "\rNext reminder in: " << i << " seconds...   " << flush;
+            
+            this_thread::sleep_for(chrono::seconds(1));
+        }
+
+        // Clear the countdown line and notify
+        cout << "\rReminder! Playing sound...                  " << endl;
+
+        // Play sound (SND_ASYNC lets the code continue immediately)
+        PlaySound(TEXT("harikitte-ikou-kitasan-black.wav"), NULL, SND_FILENAME | SND_ASYNC);
     }
 
     return 0;
